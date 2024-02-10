4a31bb7cd2893e034104d5bacb8429dc331c29ab
Sonal-21200 <kandarisonal21200@gamil.com>
2024-02-10 17:54:52 +0530

    backend

--- sb_frontend-main/src/features/auth/components/Login.js
+++ sb_frontend-main/src/features/auth/components/Login.js
@@ -101,7 +101,9 @@ export default function Login() {
               type="submit"
               className="inline-flex cursor-pointer items-center justify-center rounded-xl border-2 border-success bg-success px-4 py-2.5 text-sm font-semibold text-white shadow-sm hover:border-primary-accent hover:bg-primary-accent focus:outline-none focus:ring-2 focus:ring-orange-400/80 focus:ring-offset-0 disabled:opacity-30 disabled:hover:border-primary disabled:hover:bg-primary disabled:hover:text-white dark:focus:ring-white/80"
             >
+              <a href='/home'>
               Login
+              </a>
             </button>
 
             <button
